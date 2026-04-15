b3785f45aefd648f56be8db36a1419d63943dab4
MWAS-SPM <mwangi2004spm@gmail.com>
2026-04-15 18:48:36 +0300

    Add function declarations for web server handling

--- webserver.cpp
+++ webserver.cpp
@@ -1,1 +1,8 @@
-
+// web_server.h/cpp
+void setupWebServer();
+void handleRoot();
+void handleAPIStatus();
+void handleAPISchedule();
+void handleAPIAdd();
+void handleAPIDelete();
+void handleAPIRing();
